8d09bd94dadc3af6358815c6d52ef458b59ee020
Kostas Georgiadis <kongeor@gmail.com>
2018-01-23 21:50:53 +0200

    Add book and author

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipAuthorModule } from './author/author.module';
+import { JhipBookModule } from './book/book.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        JhipAuthorModule,
+        JhipBookModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
